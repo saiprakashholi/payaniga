aa7db087c8d92ddd6e44912997570adf84b3b66e
Sai Prakash <holisaiprakash@gmail.com>
2020-03-29 16:49:18 +0530

    added angular material and flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { PaCommonModule } from './shared_modules/pa-common/pa-common.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // FlexLayoutModule
+    // FlexLayoutModule,
+
+    // modules import
+    PaCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/featured_modules/home/home.module.ts
+++ src/app/featured_modules/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatSliderModule
   ]
 })
 export class HomeModule { }
